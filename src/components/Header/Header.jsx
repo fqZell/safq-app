69ab6828d14f4a188d90d7ad92fe25d4088facf1
unknown <Demo@1318-008-22.mck-ktits.ru>
2023-02-15 16:46:01 +0300

    slider

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import logo from "../../assets/allLogo.png";
+import basket from "../../assets/basket.svg";
+import account from "../../assets/account.svg";
+import Slide from "../Slider/Slide";
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="header-container">
+
+
+                        <div className="header-logo">
+                            <img src={logo} alt="logo" />
+                            <h1>SAFQ</h1>
+                        </div>
+
+                        <nav>
+
+                        <ul>
+                            <li><a href="/">Товары</a></li>
+                            <li><a href="/">Информация</a></li>
+                        </ul>
+
+                        <div className="header-icons">
+                            <img src={basket} alt="basket" />
+                            <img src={account} alt="account" />
+                        </div>
+
+                        </nav>
+
+
+                </div>
+
+                <div className="header-line"></div>
+
+                <Slide />
+            </header>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/components/Slider/Slide.jsx
@@ -0,0 +1,19 @@
+const Slide = () => {
+    return (
+        <>
+            <div className="slider-container">
+
+                <div className="slider">
+
+                    <div className="slide active">
+                        <img src="" alt="slide" />
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Slide;
