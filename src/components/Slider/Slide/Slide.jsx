92bbb6fb8ee4450bccf12b2acfca5e75238f2f1f
unknown <Demo@1302-008-22.mck-ktits.ru>
2023-02-22 15:21:57 +0300

    slider

--- src/components/Slider/Slide/Slide.jsx
+++ src/components/Slider/Slide/Slide.jsx
@@ -3,16 +3,15 @@ import { NavLink } from "react-router-dom";
 const Slide = ({ slide }) => {
     return (
         <>
-            <div className="slide-body">
-                <h1>{slide.title}</h1>
-                <p>{slide.sabtitle}</p>
-                <NavLink className="button" to={slide.button.url}>К покупкам</NavLink>
-            </div>
-            <img 
-            src={slide.banner} 
-            alt={slide.title}
-            /> 
+        <div className="slide-body">
+            <h1>{slide.title}</h1>
+            <p>{slide.sabtitle}</p>
+            <NavLink className="button" to={slide.button.url}>К покупкам</NavLink>
         </div>
+        <img 
+        src={slide.banner} 
+        alt={slide.title}
+        />
         </>
     )
 }
